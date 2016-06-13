c9de0b091e20d730c942cf5a76319c505a3b99e4
Luther <lutheroto@gmail.com>
2016-06-13 01:13:59 -0400

    Try to write some tests.

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,11 @@
 FactoryGirl.define do
+  pw = 'helloworld'
   factory :user do
-    
+    name RandomData.random_name
+    sequence(:email) { |n|
+      "user#{n}@factory.com"
+    }
+    password pw
+    password_confirmation pw
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:my_user) {
+    create :user
+  }
+
+  it {is_expected.to validate_presence_of :name}
+
+  it {is_expected.to validate_presence_of :email}
+  it {is_expected.to validate_uniqueness_of :email}
+  it {is_expected.to allow_value("user@bloccit.com").for :email}
+
+  it {is_expected.to validate_presence_of :password}
+
+  describe('POST create') {
+    it('has all attributes') {
+      expect(my_user).to have_attributes name: my_user.name, email: my_user.email, password: my_user.password
+    }
+  }
 end
