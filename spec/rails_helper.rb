b765814020a1074046511b204da549772f1c259d
Luther <lutheroto@gmail.com>
2016-06-15 14:32:15 -0400

    Configure email.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -57,4 +57,5 @@ RSpec.configure do |config|
 
   config.include Devise::TestHelpers, type: :controller
   config.include Devise::TestHelpers, type: :view
+  config.include FactoryGirl::Syntax::Methods
 end
