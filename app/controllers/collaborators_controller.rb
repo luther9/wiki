246ce6430c7977cb653dec7923ef179edd23dfe6
Luther <lutheroto@gmail.com>
2016-07-06 15:12:01 -0400

    Get collaborator paths working from the wiki view.

--- app/controllers/collaborators_controller.rb
+++ app/controllers/collaborators_controller.rb
@@ -1,13 +1,15 @@
 class CollaboratorsController < ApplicationController
   def create
     collaborator = Collaborator.new
-    collaborator.user = params[:user]
-    collaborator.wiki = params[:wiki]
+    collaborator.user_id = params[:user]
+    collaborator.wiki_id = params[:wiki]
     collaborator.save
+    redirect_to wiki_path collaborator.wiki_id
   end
 
   def destroy
-    collaborator = Collaborator.find params[:collaborator]
+    collaborator = Collaborator.find params[:id]
     collaborator.destroy
+    redirect_to wiki_path collaborator.wiki_id
   end
 end
